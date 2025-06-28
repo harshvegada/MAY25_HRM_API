703cf117fecbf40aca3cfc00295ccbadeef6843d
harshvegada <harshvegada@users.noreply.github.com>
2025-06-28 15:14:13 +0000

    deploy: 1c3aded7e0108be4e2da89783d008bab01362504

--- src/main/java/entity/createEmployee/Params.java
+++
@@ -1,9 +0,0 @@
-package entity.createEmployee;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
-
-@Builder
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class Params{
-}
